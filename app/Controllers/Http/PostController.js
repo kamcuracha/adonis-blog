990b3ed3938db4ee1f1bb487f49f1404687fd41f
Paul James Curacha <kamcuracha@yahoo.com>
2017-11-22 23:55:56 +0800

    Add sample content to posts view

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -2,7 +2,15 @@
 
 class PostController {
     async index({ view }){
-        return view.render('posts.index')
+        const posts = [
+            {title: "Post One", body: "Sample text in one body"},
+            {title: "Post Two", body: "Sample text in two body"},
+            {title: "Post Three", body: "Sample text in three body"},
+        ]
+        return view.render('posts.index', {
+            title: 'Latest Posts',
+            posts: posts
+        })
     }
 }
 
