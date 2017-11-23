b237dea8418fa3c00e71fcd5176e3e07cc80f7fb
Paul James Curacha <kamcuracha@yahoo.com>
2017-11-23 11:27:43 +0800

    Use post model in controller

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,15 +1,19 @@
 'use strict'
 
+const Post = use('App/Models/Post')
+
 class PostController {
     async index({ view }){
-        const posts = [
-            {title: "Post One", body: "Sample text in one body"},
-            {title: "Post Two", body: "Sample text in two body"},
-            {title: "Post Three", body: "Sample text in three body"},
-        ]
+        // const posts = [
+        //     {title: "Post One", body: "Sample text in one body"},
+        //     {title: "Post Two", body: "Sample text in two body"},
+        //     {title: "Post Three", body: "Sample text in three body"},
+        // ]
+        const posts = await Post.all()
+
         return view.render('posts.index', {
             title: 'Latest Posts',
-            posts: posts
+            posts: posts.toJSON()
         })
     }
 }
